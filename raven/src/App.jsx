dd6d7207d52801382ee3e646119ad7dc4b9115b9
pranavr11 <73618764+pranavr11@users.noreply.github.com>
2023-02-21 09:32:24 -0500

    Signin and Signup functionality added
    ^

--- raven/src/App.jsx
+++ raven/src/App.jsx
@@ -4,19 +4,16 @@ import './App.css'
 import {Route, Routes} from "react-router-dom"
 import Events from './Components/Events'
 import RavenTime from './Components/RavenTime'
-// function App() {
-//   return (
-//     <body>
-//       <Home />
-//     </body>
-//   )
-// }
+import SignIn from './Components/SignIn'
+import SignUp from './Components/SignUp'
 
 function App(){
   return <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="events" element={<Events />}/>
     <Route path="raventime" element={<RavenTime />}/>
+     <Route path="signin" element={<SignIn />}/> 
+     <Route path="signup" element={<SignUp />}/> 
   </Routes>
 }
 
